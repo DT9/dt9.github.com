7ff7e391fb4068419a5000950b09573b350fbbe9
Dennis <dtruong1@ualberta.ca>
2016-09-28 22:24:40 -0600

    Remove posts

--- talks/js/html5shiv.js
+++
@@ -1,5 +0,0 @@
-(function(c){
-  if(!('hidden' in c('a'))) {
-    'header nav section article aside footer hgroup'.replace(/\w+/g,function(e) { c(e); });
-  }
-})(document.createElement);
--- talks/js/slides.js
+++
@@ -1,7 +0,0 @@
----
----
-/// Combined JavaScript for slide decks
-{% include_relative plugin/head.min.js %}
-{% include_relative plugin/classList.js %}
-{% include_relative reveal.min.js %}
-{% include_relative plugin/zoom.js %}
